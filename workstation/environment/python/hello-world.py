5329f7fde27eda48e9ceb36ae83568b2cff0ae5c
Rothl <rothlmi@E0413110.local>
2018-04-20 10:19:04 +0100

    dev: Adds docker build and run script

--- workstation/environment/python/hello-world.py
+++ workstation/environment/python/hello-world.py
@@ -1,24 +1,18 @@
-print("Hi, put breakpoint here!")
-
 import findspark
 findspark.init()
-
 from pyspark import SparkContext
 from pyspark import SparkConf
 
-APP_NAME = 'my python script'
+print("Hi, put breakpoint here!")
+
+APP_NAME = 'Debug pyspark via docker!'
 conf = SparkConf().setAppName(APP_NAME)
 conf = conf.setMaster('local[*]')
 sc = SparkContext(conf=conf)
 lines = sc.textFile("/etc/hosts")
-
-somelist = [1, 2, 3, 4, 5]
-
-lines_p = sc.parallelize(somelist)
-print(lines_p)
-
 print(lines.count())
+
 lineLength = lines.map(lambda s: len(s))
 lineLength.persist()
-totalLength = lineLength.reduce(lambda a,b:a+b)
+totalLength = lineLength.reduce(lambda a, b: a + b)
 print(totalLength)
